7ef8408b60381aeda6fcb323aeb2e4b1349b5359
gauravKverma <gauravverma004@gmail.com>
2022-10-01 12:06:39 +0530

    day5-products

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Login from './Pages/Login/Login';
 import Signup from './Pages/Login/Signup';
 import Cart from './Pages/Cart/Cart';
 import Account from './Pages/AccountPage/Account';
-import Beauty from './Pages/Products/Beauty';
 import Footer from './Components/Footer/Footer';
 import Home from './Pages/Homepage/Home';
+import Navbar from './Components/Navbar/Navbar';
+import Products from './Pages/Products/Products';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/category/beauty" element={<Beauty/>}/>
+        <Route path="/products/:category" element={<Products/>}/>
         <Route path='/login' element={<Login/>} />
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/account/:id' element={<Account/>}/>
