711b780941638cae365011c025a3e5a160c06619
gauravKverma <gauravverma004@gmail.com>
2022-10-01 18:08:30 +0530

    products page complete

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -18,7 +18,7 @@ import {
 } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import AppSection from "./AppSection";
 import styles from "./products.module.css";
 import { HiOutlineSearch } from "react-icons/hi";
