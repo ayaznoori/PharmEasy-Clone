6e129a02daa25457839a75b6d8b588f01a0a1fd1
gauravKverma <gauravverma004@gmail.com>
2022-10-02 10:29:39 +0530

    completed

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -9,58 +9,151 @@ import {
   InputRightElement,
   Stack,
   Text,
+  VStack,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { OffersData } from "./OffersData";
 import styles from "./offers.module.css";
 import { GoNote } from "react-icons/go";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { BiSearchAlt } from "react-icons/bi";
+import axios from "axios";
 
 const Offers = () => {
+  const [query, setQuery] = useState("");
+  const [show, setShow] = useState(false);
+  const [data, setData] = useState([]);
+
+  const handleChange = async (e) => {
+    if(e.target.value===""){
+      return setShow(false);
+    }
+    setQuery(e.target.value);
+    setShow(true);
+    let res = await axios.get(
+      `http://localhost:8080/products?name=${e.target.value}`
+    );
+    setData(res.data);
+  };
+  
+  const element=document.querySelector("#box");
+  console.log(element);
+
+  // if()
   return (
     <div>
-      <Stack
-        width={"60%"}
-        margin="auto"
-        marginTop="32px"
-        marginBottom={"40px"}
-        gap="15px"
-      >
-        <Flex justifyContent={"space-between"}>
-          <Heading fontSize={"22px"}>What are you looking for?</Heading>
-          <Flex alignItems={"center"} gap="10px">
-            <GoNote fontSize={"24px"} />
-            <Text fontSize={"14px"} fontWeight="500">
-              Order with prescription.
-            </Text>
-            <Text fontSize={"14px"} fontWeight="700" color={"#10847E"}>
-              UPLOAD NOW
-            </Text>
-            <MdKeyboardArrowRight fontSize={"20px"} color={"#10847E"} />
+      <div className={styles.backColor}>
+        <Stack
+          width={"60%"}
+          margin="auto"
+          paddingTop="32px"
+          // marginBottom={"40px"}
+          gap="15px"
+        >
+          <Flex justifyContent={"space-between"}>
+            <Heading fontSize={"22px"}>What are you looking for?</Heading>
+            <Flex alignItems={"center"} gap="10px">
+              <GoNote fontSize={"24px"} />
+              <Text fontSize={"14px"} fontWeight="500">
+                Order with prescription.
+              </Text>
+              <Text fontSize={"14px"} fontWeight="700" color={"#10847E"}>
+                UPLOAD NOW
+              </Text>
+              <MdKeyboardArrowRight fontSize={"20px"} color={"#10847E"} />
+            </Flex>
           </Flex>
-        </Flex>
-        <InputGroup size="lg" alignItems={"center"}>
-          <InputLeftElement
-            children={<BiSearchAlt color="#9B929B" fontSize={"24px"} />}
-          />
-          <Input
-          boxShadow= "rgba(149, 157, 165, 0.2) 0px 3px 20px"
-            borderRadius={"150px"}
-            fontSize="15px"
-            fontWeight={"500"}
-            placeholder="Search for Medicines"
-          />
-          <InputRightElement
-            width={"100px"}
-            children={
-              <Button borderRadius={"50px"} colorScheme={"teal"} width="88px">
-                Search
-              </Button>
-            }
-          />
-        </InputGroup>
-      </Stack>
+          <InputGroup size="lg" alignItems={"center"}>
+            <InputLeftElement
+              children={<BiSearchAlt color="#9B929B" fontSize={"24px"} />}
+            />
+            <Input
+              boxShadow="rgba(149, 157, 165, 0.2) 0px 3px 20px"
+              borderRadius={"150px"}
+              fontSize="15px"
+              fontWeight={"500"}
+              placeholder="Search for Medicines"
+              onChange={handleChange}
+              focusBorderColor="#EEF4FF"
+              border={"1px solid #EEF4FF"}
+            />
+            <InputRightElement
+              width={"100px"}
+              children={
+                <Button borderRadius={"50px"} colorScheme={"teal"} width="88px">
+                  Search
+                </Button>
+              }
+            />
+          </InputGroup>
+        </Stack>
+      </div>
+      {show ? (
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            zIndex: "1000",
+            backgroundColor: "white",
+            // border:"1px solid #EEF4FF"
+          }}
+        >
+        <Box
+          position={"absolute"}
+          height={"425px"}
+          border="1px solid #EEF4FF"
+          width={"60%"}
+          margin="auto"
+          bg={"white"}
+          marginLeft="290px"
+          overflowY="scroll"
+          borderTop={"none"}
+          id="#box"
+        >
+          <VStack>
+            <Box
+              paddingLeft={"10px"}
+              width={"100%"}
+              backgroundColor="#EEF4FF"
+              textAlign={"left"}
+              fontSize="12px"
+              height={"40px"}
+              display="flex"
+              alignItems="center"
+              color="#4F585E"
+            >
+              Showing results for {query}
+            </Box>
+            {data.map((el) => (
+              <div
+                key={el._id}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "100%",
+                  textAlign: "left",
+                  paddingLeft: "10px",
+                }}
+              >
+                <Heading
+                  noOfLines={"1"}
+                  color="#30363C"
+                  fontSize="14px"
+                  fontWeight={"500"}
+                  height="20px"
+                >
+                  {el.name}
+                </Heading>
+                <Text color={"#6E787E"} fontSize={"12px"}>
+                  {el.storename}
+                </Text>
+              </div>
+            ))}
+          </VStack>
+        </Box>
+      </div>
+      ) :  
+      null}
       <div className={styles.container}>
         <Stack
           direction="horizontal"
